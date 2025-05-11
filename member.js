6650b3c5628d74ae2ebf6810ece56a570a0e4c73
Haider Ali <b21f0155ai018@fecid.paf-iast.edu.pk>
2025-05-11 11:53:02 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,27 @@
+function skillsMember() {
+  return (
+    <div className="skills-member">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>CSS</li>
+        <li>HTML</li>
+      </ul>
+    </div>
+  );
+}
+function projectsMember() {
+  return (
+    <div className="projects-member">
+      <h2>Projects</h2>
+      <ul>
+        <li>Project A</li>
+        <li>Project B</li>
+        <li>Project C</li>
+        <li>Project D</li>
+      </ul>
+    </div>
+  );
+}
